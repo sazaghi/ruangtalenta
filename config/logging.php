45a69e718757288ec22c8d5b810d25580930e4e6
sazaghi <zaaghisaad@gmail.com>
2025-06-03 11:52:02 +0700

    apalh

--- config/logging.php
+++ config/logging.php
@@ -89,7 +89,6 @@ return [
             'handler_with' => [
                 'host' => env('PAPERTRAIL_URL'),
                 'port' => env('PAPERTRAIL_PORT'),
-                'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
             ],
             'processors' => [PsrLogMessageProcessor::class],
         ],
